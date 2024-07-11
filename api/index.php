5614b667a6f46773ed1ab2c8a702eca35bf6bf56
Bharath-Viswam <79804086+Bharath-Viswam@users.noreply.github.com>
2024-07-11 12:46:28 +0530

    changes to  include index.php and path fixes vercel php

---
+++ api/index.php
@@ -0,0 +1,17 @@
+<?php
+$request = $_SERVER['REQUEST_URI'];
+
+switch ($request) {
+    case '/' :
+    case '' :
+        require __DIR__ . '/../index.html';
+        break;
+    case '/contact' :
+        require __DIR__ . '../contact.html';
+        break;
+    default:
+        http_response_code(404);
+        require __DIR__ . '/404.html';
+        break;
+}
+?>
